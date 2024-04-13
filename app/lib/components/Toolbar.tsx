2880edcc1f0e26818c1fc53a011a945f72c75109
Thomas Kretschmann <t.kretschmann@t-online.de>
2024-04-13 03:48:36 +0200

    better componenting

---
+++ app/lib/components/Toolbar.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from "react";
+
+export default function ToolBar({
+  children,
+}: Readonly<{
+  children: ReactNode;
+}>) {
+  return (
+    <div className="sticky top-0 pt-5 backdrop-blur-md bleed">
+      <div className="grid mb-2">{children}</div>
+      <hr />
+    </div>
+  );
+}
