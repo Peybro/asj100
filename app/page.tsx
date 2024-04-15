829b879a7b25ad39b1338f40bf71a5692c7828a0
Thomas <31886496+Peybro@users.noreply.github.com>
2024-04-16 00:34:29 +0200

    Aktualisieren von page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -211,8 +211,7 @@ export default function Home() {
                     {Array.from({ length: 3 }).map((_, i) => {
                       return (
                         <div key={i} className="space-y-1 animate-pulse">
-                          <div className="h-5 w-full rounded-sm bg-slate-500"></div>
-                          <div className="h-5 w-full rounded-sm bg-slate-500"></div>
+                          <div className="h-5 w-2/3 rounded-sm bg-slate-500"></div>
                           <div className="h-14 w-full rounded-md border border-slate-600 bg-gray-800"></div>
                         </div>
                       );
