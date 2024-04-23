784331564019e9cdec9436a5a3aae256d78cf1ef
Thomas Kretschmann <t.kretschmann@t-online.de>
2024-04-23 12:55:33 +0200

    amber

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ToastContainer } from "react-toastify";
+import { ReactNode } from "react";
 
 import "./globals.scss";
 import "react-toastify/dist/ReactToastify.css";
-import { ReactNode } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="yel">
       <body className={`${inter.className} container mb-10`}>
         {children}
         <ToastContainer />
