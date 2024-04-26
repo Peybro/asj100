e14327474f7c6976973f26fe71d5d01fea6986e0
Thomas Kretschmann <t.kretschmann@t-online.de>
2024-04-26 16:46:47 +0200

    dynamic line amount

--- app/lib/components/QuestionSkeletonLoader.tsx
+++ app/lib/components/QuestionSkeletonLoader.tsx
@@ -4,8 +4,8 @@ export default function QuestionSkeletonLoader() {
       {Array.from({ length: 3 }).map((_, i) => {
         return (
           <div key={`skeleton-key-${i}`} className="animate-pulse space-y-1">
-            <div className="h-5 w-2/3 rounded-xl bg-slate-500"></div>
-            <div className="h-5 w-full rounded-xl bg-slate-500"></div>
+            <div className="md:hidden h-5 w-2/3 rounded-xl bg-slate-500"></div>
+            <div className="h-5 w-5/6 rounded-xl bg-slate-500"></div>
             <div className="h-14 w-full rounded-md border border-slate-600 bg-gray-800"></div>
           </div>
         );
